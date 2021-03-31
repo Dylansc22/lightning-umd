c904b507e2e1946dabab0121c9871c1b5661fed3
Dylan <dylan.cobean@gmail.com>
2021-03-31 00:39:02 -0700

    Setup | NPM Install Mapbox | Boilerplate Mapbox Basemap Operational

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-alert("what up")
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidnByZWxvdmFjIiwiYSI6ImNqYzlqZjZjdDAwMXEyenJ1eDA0ZjJnanAifQ.HfZmaH2OvqcECAIi87A4Vg";
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});

597cffe7683860bb74e77e44d7a8cc070d33f13b
Dylan <dylan.cobean@gmail.com>
2021-03-31 00:44:31 -0700

    Lightning | Animate Raster Mapbox Demo Operational | Merely proving the concept to build off of

--- src/main.js
+++ src/main.js
@@ -2,9 +2,46 @@ import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoidnByZWxvdmFjIiwiYSI6ImNqYzlqZjZjdDAwMXEyenJ1eDA0ZjJnanAifQ.HfZmaH2OvqcECAIi87A4Vg";
-const map = new mapboxgl.Map({
-  container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/dark-v10",
+  maxZoom: 5.99,
+  minZoom: 4,
+  zoom: 5,
+  center: [-75.789, 41.874],
+});
+
+var frameCount = 5;
+var currentImage = 0;
+
+function getPath() {
+  return (
+    "https://docs.mapbox.com/mapbox-gl-js/assets/radar" + currentImage + ".gif"
+  );
+}
+
+map.on("load", function () {
+  map.addSource("radar", {
+    type: "image",
+    url: getPath(),
+    coordinates: [
+      [-80.425, 46.437],
+      [-71.516, 46.437],
+      [-71.516, 37.936],
+      [-80.425, 37.936],
+    ],
+  });
+  map.addLayer({
+    id: "radar-layer",
+    type: "raster",
+    source: "radar",
+    paint: {
+      "raster-fade-duration": 0,
+    },
+  });
+
+  setInterval(function () {
+    currentImage = (currentImage + 1) % frameCount;
+    map.getSource("radar").updateImage({ url: getPath() });
+  }, 200);
 });
